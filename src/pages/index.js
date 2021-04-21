4ae33e85118d674efb0f0e5de57aeddd2da3b91c
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-04-21 15:01:46 +0900

    Remove TextCropper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import BannerForNoJs from 'src/components/BannerForNoJs';
 import Main from 'src/blocks/Main';
 import Section from 'src/blocks/Section';
-import TextCropper from 'src/components/TextCropper';
 import TopAppBar from 'src/components/TopAppBar';
 
 function HomePage() {
@@ -20,9 +19,6 @@ function HomePage() {
       <noscript>
         <BannerForNoJs />
       </noscript>
-      <div>
-        <TextCropper />
-      </div>
     </>
   );
 }
