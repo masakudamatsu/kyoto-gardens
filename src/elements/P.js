54a63855519dab5a2ee1ccb8f53b570567fabae8
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-04-22 18:13:51 +0900

    Remove redundant code

--- src/elements/P.js
+++ src/elements/P.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import {breakpoint, paragraph, whitespace} from 'src/utils/designSpec';
+import {breakpoint, paragraph} from 'src/utils/designSpec';
 import {getFontSize, getLineHeight} from 'src/utils/fontCssFactory';
 import remify from 'src/utils/remify';
 
