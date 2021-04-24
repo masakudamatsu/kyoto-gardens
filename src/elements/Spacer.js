bc1967e311da994e687d07d39b9e1b58f9c17dd3
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-04-24 18:27:57 +0900

    Refactor Spacer component
    Make it easy to increase height for desktop screens

--- src/elements/Spacer.js
+++ src/elements/Spacer.js
@@ -1,46 +1,51 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import PropTypes from 'prop-types';
 
 import {paragraph, whitespace} from 'src/utils/designSpec';
 import {getCapHeightXHeightDiff} from 'src/utils/fontCssFactory';
 import remify from 'src/utils/remify';
 
+function getHeight(props, screenWidth = 'mobile') {
+  let targetHeight;
+  if (props.betweenLines) {
+    targetHeight = whitespace(screenWidth).betweenLines;
+  }
+  if (props.betweenParagraphs) {
+    targetHeight = whitespace(screenWidth).betweenParagraphs;
+  }
+  if (props.betweenSections) {
+    targetHeight = whitespace(screenWidth).betweenSections;
+  }
+  if (!targetHeight) {
+    throw new Error(`Spacer component does not get any props`);
+  }
+  if (props.textOnly) {
+    const spaceByDefault =
+      whitespace(screenWidth).betweenLines -
+      getCapHeightXHeightDiff(paragraph[screenWidth]);
+    return remify(targetHeight - spaceByDefault);
+  }
+  if (props.aboveFigure) {
+    const spaceByDefault = 8; // given x-height of 8px and line-height of 28px
+    return remify(targetHeight - spaceByDefault);
+  }
+  if (props.belowFigure) {
+    const spaceByDefault = 12 - getCapHeightXHeightDiff(paragraph[screenWidth]);
+    return remify(targetHeight - spaceByDefault);
+  }
+  if (props.aboveSubsectionTitle) {
+    const spaceByDefault = 14;
+    return remify(targetHeight - spaceByDefault);
+  }
+  if (props.belowSubsectionTitle) {
+    const spaceByDefault = 6;
+    return remify(targetHeight - spaceByDefault);
+  }
+}
+
 const Spacer = styled.div`
   height: ${props => {
-    let targetHeight;
-    if (props.betweenLines) {
-      targetHeight = whitespace().betweenLines;
-    }
-    if (props.betweenParagraphs) {
-      targetHeight = whitespace().betweenParagraphs;
-    }
-    if (props.betweenSections) {
-      targetHeight = whitespace().betweenSections;
-    }
-    if (!targetHeight) {
-      throw new Error(`Spacer component does not get any props`);
-    }
-    if (props.textOnly) {
-      const spaceByDefault =
-        whitespace().betweenLines - getCapHeightXHeightDiff(paragraph.mobile);
-      return remify(targetHeight - spaceByDefault);
-    }
-    if (props.aboveFigure) {
-      const spaceByDefault = 8; // given x-height of 8px and line-height of 28px
-      return remify(targetHeight - spaceByDefault);
-    }
-    if (props.belowFigure) {
-      const spaceByDefault = 12 - getCapHeightXHeightDiff(paragraph.mobile);
-      return remify(targetHeight - spaceByDefault);
-    }
-    if (props.aboveSubsectionTitle) {
-      const spaceByDefault = 14;
-      return remify(targetHeight - spaceByDefault);
-    }
-    if (props.belowSubsectionTitle) {
-      const spaceByDefault = 6;
-      return remify(targetHeight - spaceByDefault);
-    }
+    return getHeight(props, 'mobile');
   }};
   width: 100%;
 `;
--- src/elements/Spacer.test.js
+++ src/elements/Spacer.test.js
@@ -4,6 +4,51 @@ import {render, screen} from '@testing-library/react';
 import Spacer from './Spacer';
 
 describe('Props work as intended', () => {
+  test('betweenLines with aboveFigure', () => {
+    const {container} = render(<Spacer betweenLines aboveFigure />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        height: 0.8281rem;
+        width: 100%;
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
+  test('betweenLines with belowFigure', () => {
+    const {container} = render(<Spacer betweenLines belowFigure />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        height: 0.8918rem;
+        width: 100%;
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
+  test('betweenLines with belowSubsectionTitle', () => {
+    const {container} = render(<Spacer betweenLines belowSubsectionTitle />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        height: 0.9531rem;
+        width: 100%;
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
   test('betweenParagraphs with textOnly', () => {
     const {container} = render(<Spacer betweenParagraphs textOnly />);
     expect(container).toMatchInlineSnapshot(`
@@ -19,11 +64,43 @@ describe('Props work as intended', () => {
       </div>
     `);
   });
-  test('betweenSections with textOnly', () => {
-    const {container} = render(<Spacer betweenSections textOnly />);
+  test('betweenParagraphs with belowFigure', () => {
+    const {container} = render(<Spacer betweenParagraphs belowFigure />);
     expect(container).toMatchInlineSnapshot(`
       .c0 {
-        height: 2.6748rem;
+        height: 1.7772rem;
+        width: 100%;
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
+  test('betweenParagraphs with aboveSubsectionTitle', () => {
+    const {container} = render(
+      <Spacer betweenParagraphs aboveSubsectionTitle />,
+    );
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        height: 1.3385rem;
+        width: 100%;
+      }
+
+      <div>
+        <div
+          class="c0"
+        />
+      </div>
+    `);
+  });
+  test('betweenSections with aboveFigure', () => {
+    const {container} = render(<Spacer betweenSections aboveFigure />);
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        height: 3.1892rem;
         width: 100%;
       }
 
