6ec375439620ba4c5472f848512b6535bb76bdcd
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-04-30 21:36:42 +0900

    Show a placeholder while lazy-loading an image

--- src/elements/ImageWithPlaceholder.js
+++ src/elements/ImageWithPlaceholder.js
@@ -1,10 +1,90 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
+import {useState} from 'react';
 import Image from 'next/image';
 
 import {color} from 'src/utils/specColor';
+import {getFontSize, getLineHeight} from 'src/utils/fontCssFactory';
+import {breakpoint, paragraph} from 'src/utils/designSpec';
+import remify from 'src/utils/remify';
 
-const ImageWithPlaceholder = styled(Image)`
-  background-color: ${color.imagePlaceholder};
+import P from 'src/elements/P';
+import Span from 'src/elements/Span';
+
+const Wrapper = styled.div`
+  position: relative;
 `;
+
+const circleDiameter = 3.5;
+const shippoPattern = css`
+  background-color: ${color.imagePlaceholder}; /* fall back for Opera Mini */
+  background-image: radial-gradient(
+      circle closest-side,
+      transparent 91%,
+      #ffffff 100%,
+      transparent
+    ),
+    radial-gradient(
+      circle closest-side,
+      ${color.imagePlaceholder} 91%,
+      #ffffff 100%,
+      ${color.imagePlaceholder}
+    ); /* Higher than these percentages will create noticeable dots at the intersection of four circles */
+  background-position: 0 0, ${circleDiameter / 2}rem ${circleDiameter / 2}rem; /* The second pair of values should be half as large as the background-size value so that the grid of circles overlaps just in halfway */
+  background-repeat: repeat;
+  background-size: ${circleDiameter}rem ${circleDiameter}rem; /* Circles won't be recognizable if smaller than these values. The feel of textile will lose if larger than these values */
+`;
+
+const Placeholder = styled(P)`
+  ${shippoPattern}
+  align-items: center;
+  bottom 0;
+  display: flex;
+  flex-direction: column;
+  font-family: ${paragraph.mobile.fontFamily};
+  font-size: ${remify(getFontSize(paragraph.mobile))};
+  font-weight: ${paragraph.mobile.fontWeight};
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  line-height: ${getLineHeight(paragraph.mobile)};
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+  @media only screen and ${breakpoint.fontSize} {
+    font-size: ${remify(getFontSize(paragraph.desktop))};
+    line-height: ${getLineHeight(paragraph.desktop)};
+  }
+`;
+
+const ImageStyled = styled(({loaded, ...props}) => <Image {...props} />)`
+  opacity: ${({loaded}) => (loaded ? '1' : '0')};
+  transition: opacity 500ms linear;
+`; // https://github.com/styled-components/styled-components/issues/1198#issuecomment-425650423
+
+const ImageWithPlaceholder = ({src, alt, ...props}) => {
+  // https://codebrahma.com/how-to-smoothly-render-images-in-react-app/
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const [isSrcValid, setIsSrcValid] = useState(!!src);
+  const loadingMessage = <Placeholder>Loading...</Placeholder>;
+  const errorMessage = (
+    <Placeholder>
+      {alt} <Span smallcaps>(We’re sorry for failing to load the image.)</Span>
+    </Placeholder>
+  );
+  return (
+    <Wrapper data-testid="image-wrapper">
+      <ImageStyled
+        loaded={imageLoaded}
+        onError={() => setIsSrcValid(false)}
+        onLoad={() => setImageLoaded(true)}
+        src={src}
+        alt={alt}
+        {...props}
+      />
+      {imageLoaded ? null : isSrcValid ? loadingMessage : errorMessage}
+    </Wrapper>
+  );
+};
 
 export default ImageWithPlaceholder;
--- src/elements/ImageWithPlaceholder.test.js
+++ src/elements/ImageWithPlaceholder.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
 
 import ImageWithPlaceholder from './ImageWithPlaceholder';
 
@@ -7,38 +7,132 @@ const mockProps = {
   src: '/dummy.jpg',
   width: 100,
   height: 100,
+  alt: 'alt-text',
 };
 
-test('renders UI correctly:', () => {
-  const {container} = render(<ImageWithPlaceholder {...mockProps} />);
-  expect(container).toMatchInlineSnapshot(`
-    .c0 {
-      background-color: rgb(240,240,240);
-    }
+describe('renders the UI correctly during the loading time', () => {
+  beforeEach(() => {
+    render(<ImageWithPlaceholder {...mockProps} />);
+  });
+  test('Wrapper', () => {
+    expect(screen.getByTestId('image-wrapper')).toHaveStyle(
+      `position:relative;`,
+    );
+  });
+  test('Image component', () => {
+    expect(screen.getByAltText('alt-text')).toHaveStyle(`
+      opacity: 0;
+      transition: opacity 500ms linear;
+    `);
+  });
+  test('Placeholder', () => {
+    expect(screen.getByText('Loading...')).toMatchInlineSnapshot(`
+      .c0 {
+        font-family: 'Cormorant Garamond','Times New Roman',serif;
+        font-size: 1.3348rem;
+        font-weight: 600;
+        line-height: 1.393;
+        margin: 0 auto;
+        max-width: 38.9424rem;
+        padding-left: 1.3281rem;
+        padding-right: 1.3281rem;
+        padding-top: NaNrem;
+        text-align: left;
+      }
 
-    <div>
-      <div
-        style="display: inline-block; max-width: 100%; overflow: hidden; position: relative; box-sizing: border-box; margin: 0px;"
+      .c1 {
+        background-color: rgb(240,240,240);
+        background-image: radial-gradient( circle closest-side,transparent 91%,#ffffff 100%,transparent ),radial-gradient( circle closest-side,rgb(240,240,240) 91%,#ffffff 100%,rgb(240,240,240) );
+        background-position: 0 0,1.75rem 1.75rem;
+        background-repeat: repeat;
+        background-size: 3.5rem 3.5rem;
+        -webkit-align-items: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        align-items: center;
+        bottom: 0;
+        display: -webkit-box;
+        display: -webkit-flex;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-flex-direction: column;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        font-family: 'Cormorant Garamond','Times New Roman',serif;
+        font-size: 1.3348rem;
+        font-weight: 600;
+        height: 100%;
+        -webkit-box-pack: center;
+        -webkit-justify-content: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        left: 0;
+        line-height: 1.393;
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 100%;
+      }
+
+      @media only screen and (min-width:31.6802rem) {
+        .c0 {
+          padding-left: 2.2135rem;
+          padding-right: 2.2135rem;
+        }
+      }
+
+      @media only screen and (min-width:728px) {
+        .c0 {
+          font-size: 1.6489rem;
+          line-height: 1.4925;
+          max-width: 48.6522rem;
+          padding-left: 3.0078rem;
+          padding-right: 3.0078rem;
+          padding-top: NaNrem;
+        }
+      }
+
+      @media only screen and (min-width:728px) {
+        .c1 {
+          font-size: 1.6489rem;
+          line-height: 1.4925;
+        }
+      }
+
+      <p
+        class="c0 c1"
       >
-        <div
-          style="box-sizing: border-box; display: block; max-width: 100%;"
-        >
-          <img
-            alt=""
-            aria-hidden="true"
-            role="presentation"
-            src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
-            style="max-width: 100%; display: block; margin: 0px; padding: 0px;"
-          />
-        </div>
-        <noscript />
-        <img
-          class="c0"
-          decoding="async"
-          src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
-          style="position: absolute; top: 0px; left: 0px; bottom: 0px; right: 0px; box-sizing: border-box; padding: 0px; margin: auto; display: block; width: 0px; height: 0px; min-width: 100%; max-width: 100%; min-height: 100%; max-height: 100%;"
-        />
-      </div>
-    </div>
-  `);
+        Loading...
+      </p>
+    `);
+  });
+});
+
+describe('After the image is successfully loaded', () => {
+  beforeEach(() => {
+    render(<ImageWithPlaceholder {...mockProps} />);
+    fireEvent.load(screen.getByAltText('alt-text'));
+  });
+  test('Image component gets rendered', () => {
+    expect(screen.getByAltText('alt-text')).toHaveStyle(`
+      opacity: 1;
+    `);
+  });
+  test('Loading message disappears', async () => {
+    await waitFor(() => {
+      expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe('After the image fails to be loaded', () => {
+  beforeEach(() => {
+    render(<ImageWithPlaceholder {...mockProps} />);
+    fireEvent.error(screen.getByAltText('alt-text'));
+  });
+  test('Alt text is shown', async () => {
+    await waitFor(() => {
+      expect(screen.getByText(mockProps.alt)).toBeVisible();
+    });
+  });
 });
