41a2e6157085414a1e9367981089dcebd1b02dcd
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-05-14 16:43:08 +0900

    Refactor H3 component
    make it easy to reuse the component for other pages

--- src/elements/H3.js
+++ src/elements/H3.js
@@ -7,24 +7,24 @@ import {ryoanji} from 'src/utils/specRyoanji';
 import {horizontalSpacing} from 'src/utils/horizontalSpacing';
 import Main from 'src/blocks/Main';
 
-const fontStyle = {
-  ryoanji: css`
-    color: ${ryoanji.h3.color};
-    font-family: ${ryoanji.h3.fontFamily};
-    font-size: ${remify(ryoanji.h3.fontSize.mobile)};
-    font-weight: ${ryoanji.h3.fontWeight};
-    letter-spacing: ${ryoanji.h3.letterSpacing};
-    line-height: ${ryoanji.h3.lineHeight};
-    text-transform: ${ryoanji.h3.textTransform};
+function fontStyle(spec) {
+  return css`
+    color: ${spec.h3.color};
+    font-family: ${spec.h3.fontFamily};
+    font-size: ${remify(spec.h3.fontSize.mobile)};
+    font-weight: ${spec.h3.fontWeight};
+    letter-spacing: ${spec.h3.letterSpacing};
+    line-height: ${spec.h3.lineHeight};
+    text-transform: ${spec.h3.textTransform};
     @media only screen and ${breakpoint.fontSize} {
-      font-size: ${remify(ryoanji.h3.fontSize.desktop)};
+      font-size: ${remify(spec.h3.fontSize.desktop)};
     }
-  `,
-};
+  `;
+}
 
 const H3 = styled.h3`
   ${Main.Ryoanji} & {
-    ${fontStyle['ryoanji']}
+    ${fontStyle(ryoanji)}
     ${horizontalSpacing.text['ryoanji'].innerMerged}
     ${horizontalSpacing.text['ryoanji'].outer}
   }
