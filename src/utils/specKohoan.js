fd2c4b1e1331e902217cf90dfe2af2b3aae447f4
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-06-01 10:54:20 +0900

    Remove redundant code

--- src/utils/specKohoan.js
+++ src/utils/specKohoan.js
@@ -1,15 +1,5 @@
-import {xHeight} from './designSpec';
-
 export const kohoan = {
   title: 'Koho-an Temple | 孤篷庵',
   description:
     'Koho-an Temple in Kyoto is a 17th-century masterpiece of spatial design, seemingly integrating architecture with gardens to take the visitor to an imaginary boat trip at sunset.',
-  figure: {
-    maxHeight: 703, // half the height of spring/summer photos
-    maxWidth: 941, // half the width of spring/summer/autumn/winter photos
-    spaceBelowByBug: {
-      mobile: 7,
-      desktop: 10,
-    }, // See issue #29
-  },
 };
--- src/utils/specRyoanji.js
+++ src/utils/specRyoanji.js
@@ -4,12 +4,4 @@ export const ryoanji = {
   title: 'Ryoan-ji Rock Garden | 龍安寺石庭',
   description:
     'Ryoan-ji Rock Garden teaches us the principle of Zen Buddhism to live happily, with the layout of 15 rocks as a textbook.',
-  figure: {
-    maxHeight: 703, // half the height of spring/summer photos
-    maxWidth: 941, // half the width of spring/summer/autumn/winter photos
-    spaceBelowByBug: {
-      mobile: 8,
-      desktop: 12,
-    }, // See issue #29
-  },
 };
