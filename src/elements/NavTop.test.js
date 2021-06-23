81ca789346585386afa2501b12e839e4770d39ce
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-06-23 18:38:24 +0900

    test(navtop): add the missing tests for NavTop.Li component
    detect any changes of styling of NavTop.Li

--- src/elements/NavTop.test.js
+++ src/elements/NavTop.test.js
@@ -75,3 +75,123 @@ describe('NavTop.Ul renders UI correctly', () => {
       transform: translateX(0)`);
   });
 });
+
+describe('NavTop.Li renders UI correctly', () => {
+  test('when currentPage and pageName props are different', () => {
+    const {container} = render(
+      <NavTop.Li currentPage="kohoan" pageName="ryoanji" />,
+    );
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        background-color: inherit;
+        color: inherit;
+        border-bottom: 1px solid gray;
+        font-family: 'Reforma 1918',serif;
+        font-size: 1.3333rem;
+        font-weight: 300;
+        line-height: normal;
+        padding-bottom: 0.625rem;
+        padding-left: 1rem;
+        padding-top: 0.5313rem;
+        position: relative;
+      }
+
+      .c0 a,
+      .c0 a:visited {
+        color: inherit;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+      }
+
+      .c0 a:focus,
+      .c0 a:hover {
+        outline: none;
+      }
+
+      .c0 a::after {
+        content: '>';
+        height: 100%;
+        left: 0;
+        padding-bottom: 0.625rem;
+        padding-right: 1rem;
+        padding-top: 0.5313rem;
+        position: absolute;
+        text-align: right;
+        top: 0;
+        width: 100%;
+      }
+
+      .c0 a:focus::after,
+      .c0 a:hover::after {
+        background-color: hsla(193,30%,57%,0.2);
+      }
+
+      <div>
+        <li
+          class="c0"
+        />
+      </div>
+    `);
+  });
+  test('when currentPage and pageName props are the same', () => {
+    const {container} = render(
+      <NavTop.Li
+        currentPage="kohoan"
+        pageName="kohoan"
+        data-testid="destination"
+      />,
+    );
+    expect(container).toMatchInlineSnapshot(`
+      .c0 {
+        background-color: white;
+        color: black;
+        border-bottom: 1px solid gray;
+        font-family: 'Reforma 1918',serif;
+        font-size: 1.3333rem;
+        font-weight: 300;
+        line-height: normal;
+        padding-bottom: 0.625rem;
+        padding-left: 1rem;
+        padding-top: 0.5313rem;
+        position: relative;
+      }
+
+      .c0 a,
+      .c0 a:visited {
+        color: inherit;
+        -webkit-text-decoration: none;
+        text-decoration: none;
+      }
+
+      .c0 a:focus,
+      .c0 a:hover {
+        outline: none;
+      }
+
+      .c0 a::after {
+        content: '';
+        height: 100%;
+        left: 0;
+        padding-bottom: 0.625rem;
+        padding-right: 1rem;
+        padding-top: 0.5313rem;
+        position: absolute;
+        text-align: right;
+        top: 0;
+        width: 100%;
+      }
+
+      .c0 a:focus::after,
+      .c0 a:hover::after {
+        background-color: transparent;
+      }
+
+      <div>
+        <li
+          class="c0"
+          data-testid="destination"
+        />
+      </div>
+    `);
+  });
+});
