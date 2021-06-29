72265830db9b8f7a040ca69e41595a53a4402e8b
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-06-29 13:53:04 +0900

    build(cypress): remove cypress-react-unit-test
    we no longer use it

--- cypress/e2e/smoketest.js
+++
@@ -1,5 +0,0 @@
-describe('Smoke Test', () => {
-  it('Makes sure the welcome message comes up', () => {
-    cy.visit('/').contains('Next.js Template');
-  });
-});
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,9 +16,6 @@
 // Cypress Testing Library
 import '@testing-library/cypress/add-commands';
 
-// Cypress React Unit Test
-import 'cypress-react-unit-test/support';
-
 // Import commands.js using ES2015 syntax:
 import './commands';
 
